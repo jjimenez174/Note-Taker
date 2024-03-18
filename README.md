94fa914aa860b99b5b429085a00a84f312c2c538
Jessica Jimenez <jjimenez174@yahoo.com>
2024-03-17 21:32:27 -0700

    added dependencies, created server.js and added a readme

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Note-Taker
+
+## Description
+
+The Note-Taker helps the user write and save notes. It a great way for the user to be organize and keep track of tasks.
+
+[Link to Demo]()
+
+## Table of Content
+[Installation](#installation)  
+[Usage](#usage)  
+[Contribution](#contribution)  
+[Test](#test)  
+[Questions](#questions) 
+
+## Installation
+* Clone the repositoy
+* Open the file in VS code or any source code editor
+* Open terminal
+* run <b>npm init</b> in terminal
+* run <b>npm i express</b> in the terminal
+* run <b>npm i uniqid</b> in the terminal
+
+All set to run the app. 
+
+## Usage
+* run <b>node server</b> in the terminal
+* server would be available at <b>localhost:3001</b>
+*
+ 
+## Contribution
+At the moment I won't be accepting contributions.
+
+## Test
+
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+For more information, visit [License Details](https://opensource.org/license/MIT)
+
+## Questions
+
+Contact me at: [jjimenez174](https://github.com/jjimenez174)
+
+## Link Deploy Application
+
+[Deploy Application]()
---
+++ server.js
@@ -0,0 +1,59 @@
+// Dependencies
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+
+
+// Creates unique IDs
+var uniqid = require('uniqid');
+
+// Express app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// GET Route for homepage
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// Routing
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+        let db = JSON.parse(fs.readFileSync('db/db.json'));
+        res.json(db);
+    });
+
+    app.post('/api/notes', (req, res) => {
+        let db = JSON.parse(fs.readFileSync('db/db.json'));
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uniqid(),
+        };
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(userNote);
+    });
+
+    app.delete('/api/notes/:id', (req, res) => {
+        let db = JSON.parse(fs.readFileSync('db/db.json'));
+        let deleteNotes = db.filter(item => item.id !== req.params.id);
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+        res.json(deleteNotes);
+    });
+};
+
+// APP listener - starts the server
+app.listen(PORT, () => {
+    console.log(`Server available at localhost:${PORT}`);
+});
+
