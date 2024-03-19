0b049b021eac2f0945d553af486c4e37633543f6
Jessica Jimenez <jjimenez174@yahoo.com>
2024-03-18 17:32:07 -0700

    updated server.js

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // GET Route for homepage
-module.exports = (app) => {
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -25,37 +24,37 @@ app.get('*', (req,res) => {
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-};
+
 
 // Routing
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
-        let db = JSON.parse(fs.readFileSync('db/db.json'));
+        let db = JSON.parse(fs.readFileSync('./db/db.json'));
         res.json(db);
     });
 
     app.post('/api/notes', (req, res) => {
-        let db = JSON.parse(fs.readFileSync('db/db.json'));
+        let db = JSON.parse(fs.readFileSync('./db/db.json'));
         let userNote = {
             title: req.body.title,
             text: req.body.text,
             id: uniqid(),
         };
         db.push(userNote);
-        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        fs.writeFileSync('./db/db.json', JSON.stringify(db));
         res.json(userNote);
     });
 
     app.delete('/api/notes/:id', (req, res) => {
-        let db = JSON.parse(fs.readFileSync('db/db.json'));
+        let db = JSON.parse(fs.readFileSync('./db/db.json'));
         let deleteNotes = db.filter(item => item.id !== req.params.id);
         fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
         res.json(deleteNotes);
     });
-};
+
 
 // APP listener - starts the server
 app.listen(PORT, () => {
     console.log(`Server available at localhost:${PORT}`);
 });
-
+};
